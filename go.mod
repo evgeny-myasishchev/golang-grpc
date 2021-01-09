17d4f31e32c263a29549d2318b7d79db9cf09f0c
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2021-01-09 23:16:11 +0200

    Adding protoc.

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/evgeny-myasishchev/golang-grpc
 
 go 1.15
+
+require (
+	github.com/golang/protobuf v1.4.3
+	google.golang.org/protobuf v1.25.0 // indirect
+)
---
+++ tools/tools.go
@@ -0,0 +1,10 @@
+// +build tools
+
+// See https://github.com/go-modules-by-example/index/blob/master/010_tools/README.md
+// for some notes on this file
+
+package tools
+
+import (
+	_ "github.com/golang/protobuf/protoc-gen-go"
+)
