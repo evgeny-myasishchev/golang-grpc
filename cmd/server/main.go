5ccab378c267e0ab1b5872d8a36ca4beda16dcf3
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2021-01-10 23:14:32 +0200

    Intercepters.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -28,6 +28,16 @@ func (s *server) GetMessages(_ *chat.GetMessagesRequest, out chat.ChatService_Ge
 	return nil
 }
 
+func unaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	log.Printf("message: %v", info.FullMethod)
+	return handler(ctx, req)
+}
+
+func streamInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	log.Printf("stream: %v", info.FullMethod)
+	return handler(srv, ss)
+}
+
 func main() {
 
 	lis, err := net.Listen("tcp", ":9000")
@@ -35,7 +45,10 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(
+		grpc.ChainUnaryInterceptor(unaryInterceptor),
+		grpc.ChainStreamInterceptor(streamInterceptor),
+	)
 	chat.RegisterChatServiceServer(grpcServer, &server{})
 
 	if err := grpcServer.Serve(lis); err != nil {
